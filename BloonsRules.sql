a6ea0b7bbe6b05b3ee884b9d748db5f6a341b509
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2019-07-23 14:34:55 -0400

    Update BloonsRules.sql

--- BloonsRules.sql
+++ BloonsRules.sql
@@ -1,13 +1,11 @@
-select
-Rule1||’_‘||Rule2||’_‘||Rule3 as rules
+-- rules / date / number of players / total plays
+select Rule1||''||Rule2||''||Rule3 as rules
 ,created_at::Date as date
 ,count(distinct userid) as players
 ,count(userid) as plays
 //,min(created_at) as start
 //,max(created_at) as endtime
-
 from prod_games.bloons.BATMOBILE_STARTMARSTRACK
-where created_at::Date >= ‘6/20/2019’
+where created_at::Date >= '6/20/2019'
 group by 1,2
-having count(distinct userid) > 500
-;
+having count(distinct userid) > 500;
