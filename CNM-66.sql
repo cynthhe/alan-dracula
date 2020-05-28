aac2205c8435f4e1a5c44b06ad3508fd38af0913
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-05-28 16:52:52 -0400

    Update CNM-66.sql

--- CNM-66.sql
+++ CNM-66.sql
@@ -4,7 +4,8 @@ use warehouse wh_default;
 
 -- create capture view
 create view capture as
-select distinct acr_capture.userid||acr_capture.sessionid||acr_capture.submit_time as id
+select distinct acr_capture.userid||acr_capture.sessionid||acr_capture.submit_time as id,
+userid, sessionid, submit_time, platform, city, country, success
 from acr_capture;
 
 -- drop capture view
@@ -13,7 +14,7 @@ drop view capture;
 -- create result view
 create view result as
 select distinct acr_result.userid||acr_result.sessionid||acr_result.submit_time as id,
-acr_result.code
+success, acr_result.code
 from acr_result;
 
 -- drop result view
@@ -25,3 +26,9 @@ from result
 join capture on (result.id = capture.id)
 group by 1
 order by result.code;
+
+-- Regina's Charles log (user id: db775dd8-99a8-4de9-973a-da0a2c3adb20)
+select *
+from capture
+join result on (capture.id = result.id)
+having userid = 'db775dd8-99a8-4de9-973a-da0a2c3adb20';
