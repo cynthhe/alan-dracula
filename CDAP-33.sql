95aa4d20b59fa112188ff2d440b901153aef0b30
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-06-02 12:16:13 -0400

    Update CDAP-33.sql

--- CDAP-33.sql
+++ CDAP-33.sql
@@ -2,45 +2,6 @@ use database prod_games;
 use schema arcade;
 use warehouse wh_default;
 
--- max time in app (seconds) for each user
-select userid, sessionid, submit_time::Date as date, max(duration) as time_in_app
-from apprunning
-group by 1,2,3
-limit 100;
-
--- date range
-select min(submit_time::Date) as min_date, max(submit_time::Date) as max_date
-from apprunning;
-
--- duration range
-select min(duration) as min_duration, max(duration) as max_duration
-from apprunning;
-
--- number of sessions for each user
-select distinct userid, count(sessionid)
-from apprunning
-group by 1;
-
--- user with highest duration
-select userid, sessionid, submit_time, duration as time_in_app
-from apprunning
-where userid = '94d043b89f89c4b57b144dedf7a0d350'
-order by submit_time;
-
--- segments within a 1 hour range
-select case
-when duration between 1 and 720 then '1-720'
-when duration between 721 and 1440 then '721-1440'
-when duration between 1441 and 2160 then '1441-2160'
-when duration between 2160 and 2880 then '2160-2880'
-when duration between 2880 and 3600 then '2880-3600'
-else 'OTHERS'
-end as range_sec,
-count(1) as count_
-from apprunning
-group by range_sec
-order by range_sec asc;
-
 -- if a session is quiet for more than 20 minutes, add something to the session id to make it a new session
 create view arcadesessions as
 with session_run as (
@@ -49,17 +10,19 @@ with session_run as (
 lag_events as ( -- events occurring on the same day and with the same user id
   select
   userid,
-  submit_time,
-  lag(submit_time) over (partition by date(submit_time), userid order by submit_time) as prev -- partition date and user id
+  sessionid,
+  ts,
+  lag(ts) over (partition by date(ts), userid order by ts) as prev -- partition date and user id
   from session_run
 ),
 new_sessions as ( -- determines whether start of a new session
   select
   userid,
-  submit_time,
+  sessionid,
+  ts,
   case -- compares current event time with the previous event time for that user
   when prev is null then 1 -- new session (first event by that user on that day)
-  when timediff(minute, submit_time, prev) < -20 then 1 -- new session (if more than 20 minutes has elapsed)
+  when timediff(minute, ts, prev) < -20 then 1 -- new session (if more than 20 minutes has elapsed)
   else 0 -- not a new session
   end as is_new_session -- creates new column 'is_new_session'
   from lag_events
@@ -67,29 +30,50 @@ new_sessions as ( -- determines whether start of a new session
 session_index as ( -- incrementing each time a new session is found for a given user
   select
   userid,
-  submit_time,
+  sessionid,
+  ts,
   is_new_session,
-  sum(is_new_session) over (partition by userid order by submit_time rows between unbounded preceding and current row) as session_index -- # of sessions for given user
+  sum(is_new_session) over (partition by userid order by ts rows between unbounded preceding and current row) as session_index -- # of sessions for given user
   from new_sessions
 )
 select
-cast(userid as varchar) || '-' || cast(session_index as varchar) as session_id, -- creates new session id (concat user id + session index)
+distinct userid || sessionid || ts  as session_id, -- creates new session id (concat user id + submit time)
 userid,
-submit_time,
+sessionid,
+ts,
 is_new_session,
 session_index
 from session_index
-order by userid, submit_time;
+order by userid, sessionid, ts;
 
 -- drop arcadesessions view
 drop view arcadesessions;
 
--- time in app per session
-select distinct userid, sessionid, submit_time::Date as date, round(max(duration) / 60) as time_in_app
+-- create arcadedurations view
+create view arcadedurations as
+select distinct userid || sessionid || ts  as session_id,
+max(duration) as time_in_app
 from apprunning
-group by 1,2,3
-having time_in_app between 1 and 20 -- users who have played b/w 1-20 min
-order by random() limit 1000; -- get simple random sample of 1000
+group by 1;
+
+-- drop arcadedurations view
+drop view arcadedurations;
+
+-- arcadesessions and arcadedurations joined
+create view arcade_session as
+select userid, sessionid, ts::date as date, is_new_session, session_index, round(time_in_app / 60) as duration
+from arcadesessions
+join arcadedurations on (arcadesessions.session_id = arcadedurations.session_id);
+
+-- drop arcade_session view
+drop view arcade_session;
+
+-- time in app per session
+select distinct userid, sessionid, date, duration
+from arcade_session
+group by 1,2,3,4
+having duration between 1 and 20 -- users who have played b/w 1-20 min
+order by random() limit 20000; -- get simple random sample of 1000
 
 -- time in app per session (COVID)
 select distinct userid, sessionid, submit_time::Date as date, round(max(duration) / 60) as time_in_app
@@ -111,3 +95,24 @@ from apprunning
 group by 1,2
 having time_in_app between 1 and 20 and date > '2020-02-29'; -- users who have played an avg b/w 1-20 min + starting from 3/1/20
 order by random() limit 1000; -- get simple random sample of 1000
+
+-- segmenting groups
+select distinct case
+when duration between 0 and 4 then 'Not engaged'
+when duration between 4 and 8 then 'Engaged'
+when duration >= 8 then 'Ultra engaged'
+else 'OTHERS'
+end as segments,
+count(1) as num_users,
+concat(round(((num_users / (select count(*) from apprunning)) * 100), 2), '%') as "% of Total"
+from arcade_perday
+group by segments
+order by num_users desc;
+
+-- create arcade_perday view
+create view arcade_perday as
+select distinct session_index, userid, sessionid, date, is_new_session, duration
+from arcade_session;
+
+-- drop arcade_perday view
+drop view arcade_perday;
