e35d068a27cfb5de3f30b2fd4ae992a061b109c3
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-06-10 12:50:40 -0400

    Update CNM-66.sql

--- CNM-66.sql
+++ CNM-66.sql
@@ -1,34 +1,45 @@
-use database prod_games;
-use schema arcade;
-use warehouse wh_default;
+USE DATABASE prod_games;
+USE SCHEMA arcade;
+USE WAREHOUSE wh_default;
 
--- create capture view
-create view capture as
-select distinct acr_capture.userid||acr_capture.sessionid||acr_capture.submit_time as id,
-userid, sessionid, submit_time, platform, city, country, success
-from acr_capture;
+-- Create capture view
+CREATE VIEW capture AS
+SELECT DISTINCT 
+    acr_capture.userid||acr_capture.sessionid||acr_capture.submit_time AS id,
+    userid, 
+    sessionid, 
+    submit_time, 
+    platform, 
+    city, 
+    country, 
+    success
+FROM acr_capture;
 
--- drop capture view
-drop view capture;
+-- Drop capture view
+DROP VIEW capture;
 
--- create result view
-create view result as
-select distinct acr_result.userid||acr_result.sessionid||acr_result.submit_time as id,
-success, acr_result.code
-from acr_result;
+-- Create result view
+CREATE VIEW result AS
+SELECT DISTINCT 
+    acr_result.userid||acr_result.sessionid||acr_result.submit_time AS id, 
+    success, 
+    acr_result.code
+FROM acr_result;
 
--- drop result view
-drop view result;
+-- Drop result view
+DROP VIEW result;
 
--- get the rate of each code firing | capture and result views joined on id
-select distinct result.code, count(*)
-from result
-join capture on (result.id = capture.id)
-group by 1
-order by result.code;
+-- Get the rate of each code firing | capture and result views joined on ID
+SELECT DISTINCT 
+    result.code, 
+    COUNT(*)
+FROM result
+JOIN capture ON (result.id = capture.id)
+GROUP BY 1
+ORDER BY result.code;
 
 -- Regina's Charles log (user id: db775dd8-99a8-4de9-973a-da0a2c3adb20)
-select *
-from capture
-join result on (capture.id = result.id)
-having userid = 'db775dd8-99a8-4de9-973a-da0a2c3adb20';
+SELECT *
+FROM capture
+JOIN result ON (capture.id = result.id)
+HAVING userid = 'db775dd899a84de9973ada0a2c3adb20';
