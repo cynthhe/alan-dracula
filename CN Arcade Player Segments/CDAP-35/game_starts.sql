c90bf4ef5a484e8a2fd88dbb8340dfb71a7a5a47
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-07-01 14:56:24 -0400

    Create game_starts.sql

---
+++ CN Arcade Player Segments/CDAP-35/game_starts.sql
@@ -0,0 +1,46 @@
+USE DATABASE prod_games;
+USE SCHEMA arcade;
+USE warehouse wh_default;
+
+-- Creates segment_gamestarts view
+CREATE VIEW segment_gamestarts AS
+SELECT
+    a.submit_time::DATE AS date
+    ,CASE WHEN a.game_name LIKE 'Smashy%' THEN 'Smashy Pinata' ELSE a.game_name END AS game_name
+    ,a.platform
+    ,b.segment
+    ,COUNT(DISTINCT a.userid) AS users
+    ,COUNT(DISTINCT a.sessionid) AS sessions
+    ,COUNT(DISTINCT a.game_session_id) AS game_starts
+FROM prod_games.arcade.game_open a
+JOIN arcade_engagement_segments b ON (a.userid = b.userid) AND ((YEAR(a.submit_time)||LPAD(MONTH(a.submit_time),2,'0')) = b.yearmonth)
+WHERE a.country LIKE 'US' 
+AND a.userid IN (SELECT userid 
+                 FROM prod_games.arcade.FIRST_PLAYED_DATE 
+                 WHERE START_DATE >= '3/4/2019')
+AND date >= '3/4/2019'
+GROUP BY 1,2,3,4;
+
+-- Drops segment_gamestarts view
+DROP VIEW segment_gamestarts;
+
+-- Creates active_game_gamestarts
+CREATE VIEW active_game_gamestarts AS
+SELECT
+    a.submit_time::DATE AS date
+    ,CASE WHEN b.active_game LIKE 'Smashy%' THEN 'Smashy Pinata' ELSE b.active_game END AS active_game
+    ,a.platform
+    ,COUNT(DISTINCT a.userid) AS users
+    ,COUNT(DISTINCT a.sessionid) AS sessions
+    ,COUNT(DISTINCT a.game_session_id) AS game_starts
+FROM prod_games.arcade.game_open a
+JOIN arcade_active_game b ON (a.userid = b.userid) AND (a.submit_time::DATE = b.date)
+WHERE a.country LIKE 'US' 
+AND a.userid IN (SELECT userid 
+                 FROM prod_games.arcade.FIRST_PLAYED_DATE 
+                 WHERE START_DATE >= '3/4/2019')
+AND date >= '3/4/2019'
+GROUP BY 1,2,3;
+
+-- Drops active_game_gamestarts
+DROP VIEW active_game_gamestarts;
