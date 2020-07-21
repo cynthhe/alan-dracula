897618564e7f6af131b2dfcbb20db624c8283f28
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-07-21 19:10:57 -0400

    Update wow_retention.sql

--- CN Arcade Player Segments/CDAP-35/wow_retention.sql
+++ CN Arcade Player Segments/CDAP-35/wow_retention.sql
@@ -5,30 +5,29 @@ USE warehouse wh_default;
 -- Creates ARCADE_SEGMENT_WOW view
 CREATE OR REPLACE VIEW ARCADE_SEGMENT_WOW AS
 SELECT
-    year||week as yearweek -- creates a week field that takes the year into account so it doesn't break as you transition from year to year.
-    ,RANK() OVER (PARTITION BY platform ORDER BY yearweek ASC) AS rank
+    year||'-'||week as yearweek -- creates a week field that takes the year into account so it doesn't break as you transition from year to year.
+    ,RANK() OVER (PARTITION BY platform ORDER BY year,week ASC) AS rank
     ,segment
     ,platform
     ,week0
     ,week1
-    ,week1/ week0 wow_retention
+    ,week1/week0 AS wow_retention
 FROM (SELECT
-        '2019' AS year
-        ,week(a.submit_time) AS week
+        YEAR(a.submit_time) AS year
+        ,WEEK(a.submit_time) AS week
         ,a.platform
         ,c.segment
         ,COUNT(DISTINCT a.userid) as week0
         ,COUNT(DISTINCT b.userid) as week1
       FROM prod_games.arcade.apprunning a
-      LEFT JOIN prod_games.arcade.apprunning b ON week(b.submit_time) = week(a.submit_time)+1 AND (a.userid = b.userid)
+      LEFT JOIN prod_games.arcade.apprunning b ON WEEK(b.submit_time) = WEEK(a.submit_time)+1 AND (a.userid = b.userid)
       JOIN prod_games.arcade.arcade_engagement_segments c ON (a.userid = c.userid) AND ((YEAR(a.submit_time)||LPAD(MONTH(a.submit_time),2,'0')) = c.yearmonth)
-      WHERE a.submit_time::date >= '3/4/2019' AND week(a.submit_time) < week(CURRENT_TIMESTAMP)+1
+      WHERE a.submit_time::DATE >= '3/4/2019' AND WEEK(a.submit_time) < WEEK(CURRENT_TIMESTAMP)+1
       AND a.userid IN (SELECT userid 
                        FROM prod_games.arcade.FIRST_PLAYED_DATE 
                        WHERE START_DATE >= '3/4/2019')
       AND a.country LIKE 'US'
-      GROUP BY 1,2,3,4
-      ORDER BY week0 DESC);
+      GROUP BY 1,2,3,4);
 
 -- Creates ARCADE_ACTIVE_GAME_WOW view
 CREATE OR REPLACE VIEW ARCADE_ACTIVE_GAME_WOW AS
@@ -49,7 +48,7 @@ FROM (SELECT
         ,COUNT(DISTINCT b.userid) as week1
       FROM prod_games.arcade.apprunning a
       LEFT JOIN prod_games.arcade.apprunning b ON week(b.submit_time) = week(a.submit_time)+1 AND (a.userid = b.userid)
-      JOIN prod_games.arcade.arcade_active_game c ON (a.userid = c.userid) AND (a.submit_time::DATE = c.date)
+      LEFT JOIN prod_games.arcade.arcade_active_game c ON (a.userid = c.userid) AND (a.submit_time::DATE = c.date)
       WHERE a.submit_time::date >= '3/4/2019' AND week(a.submit_time) < week(CURRENT_TIMESTAMP)+1
       AND a.userid IN (SELECT userid 
                        FROM prod_games.arcade.FIRST_PLAYED_DATE 
@@ -66,7 +65,7 @@ USE warehouse wh_default;
 -- Creates reporting view: ARCADE_SEGMENT_WOW_VIEW
 CREATE OR REPLACE VIEW ARCADE_SEGMENT_WOW_VIEW AS
 SELECT *
-FROM prod_games.arcade.ARCADE_SEGMENT_WOW;
+FROM prod_games.arcade.ARCADE_SEGMENT_WOW_TABLE;
 
 -- Creates reporting view: ARCADE_ACTIVE_GAME_WOW_VIEW
 CREATE OR REPLACE VIEW ARCADE_ACTIVE_GAME_WOW_VIEW AS
