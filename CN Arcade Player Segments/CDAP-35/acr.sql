4737087a0c5201cbe9175f8ccd7a280ab30060ff
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-08-07 15:20:21 -0400

    Update acr.sql

--- CN Arcade Player Segments/CDAP-35/acr.sql
+++ CN Arcade Player Segments/CDAP-35/acr.sql
@@ -2,7 +2,7 @@ USE DATABASE prod_games;
 USE SCHEMA arcade;
 USE warehouse wh_default;
 
--- Creates segment_acr view
+-- Create SEGMENT_ACR view
 CREATE OR REPLACE VIEW segment_acr AS
 SELECT
     a.userid
@@ -19,73 +19,15 @@ JOIN prod_games.arcade.arcade_engagement_segments b ON (a.userid = b.userid) AND
 WHERE a.country LIKE 'US'
 AND a.submit_time >= '3/4/2019';
 
--- Drops segment_acr view
-DROP VIEW segment_acr;
-
--- Tests segment_acr view
-SELECT *
-FROM segment_acr;
-
--- Creates ARCADE_SEGMENT_ACR table
-CREATE TABLE ARCADE_SEGMENT_ACR AS
-SELECT *
-FROM segment_acr;
-
--- Creates active_game_acr view
-CREATE OR REPLACE VIEW active_game_acr AS
-SELECT DISTINCT
-    a.userid
-    ,CASE WHEN b.active_game LIKE 'Smashy%' THEN 'Smashy Pinata' ELSE b.active_game END AS active_game
-    ,sessionid
-    ,a.submit_time
-    ,ts
-    ,platform
-    ,city
-    ,country
-    ,capture_id
-    ,capture_time
-    ,episode_name
-    ,play_userloggedin
-    ,figure_granted
-    ,success
-    ,code
-FROM prod_games.arcade.ACR a
-JOIN prod_games.arcade.arcade_active_game b ON (a.userid = b.userid) AND (a.submit_time::DATE = b.date)
-WHERE a.country LIKE 'US'
-AND a.submit_time >= '3/4/2019';
-
--- Testing active_game_acr view
-SELECT *
-FROM active_game_acr;
-
-SELECT DISTINCT 
-    code 
-    ,success
-    ,COUNT(*)
-FROM active_game_acr
-GROUP BY 1,2
-ORDER BY code;
-
--- Creates ARCADE_ACTIVE_GAME_ACR table
-CREATE TABLE ARCADE_ACTIVE_GAME_ACR AS
-SELECT *
-FROM prod_games.arcade.active_game_acr;
-
 -- REPORTING schema
 USE DATABASE prod_games;
 USE SCHEMA reporting;
 USE warehouse wh_default;
 
--- Creates reporting view: ARCADE_SEGMENT_ACR_VIEW
+-- Create reporting view: ARCADE_SEGMENT_ACR_VIEW
 CREATE OR REPLACE VIEW ARCADE_SEGMENT_ACR_VIEW AS
 SELECT *
 FROM prod_games.reporting.ARCADE_SEGMENT_ACR;
 
--- Creates reporting view: ARCADE_ACTIVE_GAME_ACR_VIEW
-CREATE OR REPLACE VIEW ARCADE_ACTIVE_GAME_ACR_VIEW AS
-SELECT *
-FROM ARCADE_ACTIVE_GAME_ACR;
-
--- Looker permissions for reporting views
+-- Looker permissions for reporting view
 GRANT SELECT ON prod_games.reporting.ARCADE_SEGMENT_ACR_VIEW TO looker_read;
-GRANT SELECT ON prod_games.reporting.ARCADE_ACTIVE_GAME_ACR_VIEW TO looker_read;
