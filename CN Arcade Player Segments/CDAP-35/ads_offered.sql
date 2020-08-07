a14707e10c2fb2547f3d91060e2109e659a8d2b7
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-08-07 15:01:34 -0400

    Update ads_offered.sql

--- CN Arcade Player Segments/CDAP-35/ads_offered.sql
+++ CN Arcade Player Segments/CDAP-35/ads_offered.sql
@@ -2,7 +2,7 @@ USE DATABASE prod_games;
 USE SCHEMA arcade;
 USE warehouse wh_default;
 
--- Creates segment_ads_offered view
+-- Create SEGMENT_ADS_OFFERED view
 CREATE OR REPLACE VIEW segment_ads_offered AS
 SELECT
     a.submit_time::DATE AS date
@@ -34,62 +34,6 @@ AND a.userid IN (SELECT userid
                WHERE START_DATE >= '3/4/2019')
 GROUP BY 1,2,3,4,5,6,7,8,9;
 
--- Drops segment_ads_offered view
-DROP VIEW segment_ads_offered;
-
--- Testing segment_ads_offered view
-SELECT *
-FROM segment_ads_offered;
-
--- Creates ARCADE_SEGMENT_ADS_OFFERED table
-CREATE TABLE ARCADE_SEGMENT_ADS_OFFERED AS
-SELECT *
-FROM prod_games.arcade.segment_ads_offered;
-
--- Creates active_game_ads_offered view
-CREATE OR REPLACE VIEW active_game_ads_offered AS
-SELECT
-    a.submit_time::DATE AS date
-    ,a.userid
-    ,CASE WHEN a.app_location LIKE 'Smashy%' THEN 'Smashy Pinata' ELSE a.app_location END AS app_location
-    ,c.active_game
-    ,a.AD_PROVIDER
-    ,b.sessions
-    ,b.players
-    ,DATEDIFF(second,OFFERED_DATETIME,FINISHED_DATETIME) AS interaction_time
-    ,a.platform
-    ,COUNT(DISTINCT a.sessionid) AS sessions_per_player
-    ,SUM(CASE WHEN a.ad_offered LIKE 'True' THEN 1 ELSE 0 END) AS ad_offered
-FROM prod_games.arcade.ad a
-LEFT JOIN (SELECT 
-            submit_time::DATE as date
-            ,platform
-            ,COUNT(DISTINCT a.userid) AS players
-            ,COUNT(DISTINCT sessionid) AS sessions 
-           FROM prod_games.arcade.apprunning a 
-           WHERE country LIKE 'US' AND a.userid IN (SELECT userid 
-                                                  FROM prod_games.arcade.FIRST_PLAYED_DATE 
-                                                  WHERE START_DATE >= '3/4/2019') 
-           GROUP BY 1,2) b ON (b.date = a.submit_time::DATE) AND (b.platform = a.platform)
-JOIN prod_games.arcade.arcade_active_game c ON (a.userid = c.userid) AND (a.submit_time::DATE = c.date)
-WHERE a.country LIKE 'US' AND a.ad_offered LIKE 'True'
-AND a.userid IN (SELECT userid 
-               FROM prod_games.arcade.FIRST_PLAYED_DATE 
-               WHERE START_DATE >= '3/4/2019')
-GROUP BY 1,2,3,4,5,6,7,8,9;
-
--- Drop active_game_ads_offered view
-DROP VIEW active_game_ads_offered;
-
--- Testing active_game_ads_offered view
-SELECT *
-FROM active_game_ads_offered;
-
--- Creates ARCADE_ACTIVE_GAME_ADS_OFFERED table
-CREATE TABLE ARCADE_ACTIVE_GAME_ADS_OFFERED AS
-SELECT *
-FROM prod_games.arcade.active_game_ads_offered;
-
 -- REPORTING schema
 USE DATABASE prod_games;
 USE SCHEMA reporting;
@@ -100,11 +44,5 @@ CREATE OR REPLACE VIEW ARCADE_SEGMENT_ADS_OFFERED_VIEW AS
 SELECT *
 FROM ARCADE_SEGMENT_ADS_OFFERED;
 
--- Creates reporting view: ARCADE_ACTIVE_GAME_ADS_OFFERED_VIEW
-CREATE OR REPLACE VIEW ARCADE_ACTIVE_GAME_ADS_OFFERED_VIEW AS
-SELECT *
-FROM ARCADE_ACTIVE_GAME_ADS_OFFERED;
-
 -- Looker permissions for reporting views
 GRANT SELECT ON prod_games.reporting.ARCADE_SEGMENT_ADS_OFFERED_VIEW TO looker_read;
-GRANT SELECT ON prod_games.reporting.ARCADE_ACTIVE_GAME_ADS_OFFERED_VIEW TO looker_read;
