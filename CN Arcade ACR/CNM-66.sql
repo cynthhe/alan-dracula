6a3c38d887c11e9433bc6218f6686bdafebb250e
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-08-07 13:36:48 -0400

    Update CNM-66.sql

--- CN Arcade ACR/CNM-66.sql
+++ CN Arcade ACR/CNM-66.sql
@@ -2,12 +2,13 @@ USE DATABASE prod_games;
 USE SCHEMA arcade;
 USE WAREHOUSE wh_default;
 
--- Create capture view
+-- Create CAPTURE view
 CREATE OR REPLACE VIEW capture AS
 SELECT DISTINCT 
     acr_capture.userid||acr_capture.sessionid||acr_capture.submit_time AS id
     ,userid
     ,sessionid
+    ,ts
     ,submit_time
     ,platform
     ,capture_time
@@ -24,15 +25,8 @@ WHERE userid IN (SELECT userid
 AND userid IN (SELECT userid 
                FROM prod_games.arcade.FIRST_PLAYED_DATE 
                WHERE START_DATE >= '3/4/2019');
-
--- Drop capture view
-DROP VIEW capture;
-
--- Testing capture view
-SELECT *
-FROM capture;
-
--- Create result view
+              
+-- Create RESULT view
 CREATE OR REPLACE VIEW result AS
 SELECT DISTINCT 
     acr_result.userid||acr_result.sessionid||acr_result.submit_time AS id 
@@ -47,20 +41,13 @@ DROP VIEW result;
 SELECT *
 FROM result;
 
--- Get the rate of each code firing | capture and result views joined on ID
-SELECT DISTINCT 
-    code 
-    ,success
-    ,COUNT(*)
-FROM ACR
-GROUP BY 1,2
-ORDER BY code;
-
 -- Create ACR view
 CREATE OR REPLACE VIEW ACR AS
 SELECT
     userid
+    ,sessionid
     ,submit_time
+    ,ts
     ,episode_name
     ,figure_granted
     ,play_userloggedin
@@ -76,25 +63,14 @@ FROM prod_games.arcade.capture
 LEFT JOIN prod_games.arcade.result ON (capture.id = result.id)
 WHERE NOT (episode_name IS NULL AND code = 0);
 
--- Testing ACR view
-SELECT *
-FROM ACR;
-
-SELECT *
+-- Get the rate of each code firing | capture and result views joined on ID
+SELECT DISTINCT 
+    code 
+    ,success
+    ,COUNT(*)
 FROM ACR
-WHERE episode_name IS NULL;
-
-SELECT *
-FROM ACR
-WHERE code IS NULL;
-
-SELECT *
-FROM ACR
-WHERE (episode_name IS NOT NULL AND code != 0);
-
-SELECT *
-FROM ACR
-WHERE result_success = 'True';
+GROUP BY 1,2
+ORDER BY code;
 
 -- Regina's Charles log (user id: d98fb8c5475f645df88dc8c2186adae3)
 SELECT DISTINCT
@@ -108,16 +84,3 @@ SELECT DISTINCT
 FROM ACR
 WHERE userid = 'd98fb8c5475f645df88dc8c2186adae3'
 AND DATE(submit_time) = '2020-05-28';
-
--- REPORTING schema
-USE DATABASE prod_games;
-USE SCHEMA reporting;
-USE warehouse wh_default;
-
--- Creates reporting view: ARCADE_SEGMENT_ACR_VIEW
-CREATE OR REPLACE VIEW ARCADE_ACR AS
-SELECT *
-FROM prod_games.arcade.ACR;
-
--- Looker permissions for reporting views
-GRANT SELECT ON prod_games.reporting.ARCADE_ACR TO looker_read;
