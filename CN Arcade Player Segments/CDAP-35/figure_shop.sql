04c0bb5d55a8bc64e61df0be665f6101695bced6
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-08-07 15:29:01 -0400

    Update figure_shop.sql

--- CN Arcade Player Segments/CDAP-35/figure_shop.sql
+++ CN Arcade Player Segments/CDAP-35/figure_shop.sql
@@ -2,7 +2,7 @@ USE DATABASE prod_games;
 USE SCHEMA arcade;
 USE warehouse wh_default;
 
--- Creates segment_figure_shop view
+-- Create SEGMENT_FIGURE_SHOP view
 CREATE OR REPLACE VIEW segment_figure_shop AS
 SELECT
     a.userid
@@ -20,49 +20,15 @@ JOIN prod_games.arcade.arcade_engagement_segments b ON (a.userid = b.userid) AND
 WHERE a.country LIKE 'US'
 AND a.submit_time >= '3/4/2019';
 
--- Creates ARCADE_SEGMENT_FIGURE_SHOP table
-CREATE TABLE ARCADE_SEGMENT_FIGURE_SHOP AS
-SELECT *
-FROM prod_games.arcade.segment_figure_shop;
-
--- Creates active_game_figure_shop view
-CREATE OR REPLACE VIEW active_game_figure_shop AS
-SELECT
-    a.userid
-    ,sessionid
-    ,a.submit_time
-    ,ts
-    ,platform
-    ,city
-    ,country
-    ,CASE WHEN b.active_game LIKE 'Smashy%' THEN 'Smashy Pinata' ELSE b.active_game END AS active_game
-    ,purchased_item_name
-    ,purchased_item_price
-FROM prod_games.arcade.purchase a
-JOIN prod_games.arcade.arcade_active_game b ON (a.userid = b.userid) AND (a.submit_time::DATE = b.date)
-WHERE a.country LIKE 'US'
-AND a.submit_time >= '3/4/2019';
-
--- Creates ARCADE_ACTIVE_GAME_FIGURE_SHOP table
-CREATE TABLE ARCADE_ACTIVE_GAME_FIGURE_SHOP AS
-SELECT *
-FROM prod_games.arcade.active_game_figure_shop;
-
 -- REPORTING schema
 USE DATABASE prod_games;
 USE SCHEMA reporting;
 USE warehouse wh_default;
 
--- Creates reporting view: ARCADE_SEGMENT_FIGURE_SHOP_VIEW
+-- Create reporting view: ARCADE_SEGMENT_FIGURE_SHOP_VIEW
 CREATE OR REPLACE VIEW ARCADE_SEGMENT_FIGURE_SHOP_VIEW AS
 SELECT *
 FROM prod_games.arcade.ARCADE_SEGMENT_FIGURE_SHOP;
 
--- Creates reporting view: ARCADE_ACTIVE_GAME_FIGURE_SHOP_VIEW
-CREATE OR REPLACE VIEW ARCADE_ACTIVE_GAME_FIGURE_SHOP_VIEW AS
-SELECT *
-FROM prod_games.arcade.ARCADE_ACTIVE_GAME_FIGURE_SHOP;
-
--- Looker permissions for reporting views
+-- Looker permissions for reporting view
 GRANT SELECT ON prod_games.reporting.ARCADE_SEGMENT_FIGURE_SHOP_VIEW TO looker_read;
-GRANT SELECT ON prod_games.reporting.ARCADE_ACTIVE_GAME_FIGURE_SHOP_VIEW TO looker_read;
