49837cb54d7d5bbccd22b981d8645e2eac1ea967
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-08-12 17:57:27 -0400

    Update segment_acr_user_journey.sql

--- segment_acr_user_journey.sql
+++ segment_acr_user_journey.sql
@@ -2,13 +2,11 @@
 CREATE VIEW segment_acr_user_journey AS
 WITH cna_journey AS 
 (SELECT DISTINCT
-    a.userid
-    ,a.sessionid
-    ,a.ts
-    ,CASE WHEN a.sessionid = b.sessionid THEN 'Yes' ELSE 'No' END AS acr_or_not
- FROM prod_games.arcade.apprunning a
- JOIN prod_games.arcade.acr_table b ON (a.userid = b.userid)
- GROUP BY 1,2,3,4)
+    userid
+    ,sessionid
+    ,ts
+ FROM prod_games.arcade.apprunning
+ GROUP BY 1,2,3)
 ,journey_data AS
 (SELECT
     userid
@@ -16,6 +14,7 @@ WITH cna_journey AS
     ,ts
     ,location
     ,segment
+    ,acr_or_not
     ,RANK() OVER (PARTITION BY sessionid ORDER BY ts ASC) AS action_sequence
  FROM (SELECT
         a.userid
@@ -23,13 +22,16 @@ WITH cna_journey AS
         ,a.ts
         ,a.screen_name AS location
         ,b.segment
+        ,CASE WHEN a.sessionid = c.sessionid THEN 'Yes' ELSE 'No' END AS acr_or_not
        FROM prod_games.arcade.screen_visit a -- screen visit, includes shop
        JOIN prod_games.arcade.engagement_segments b
        ON (a.userid = b.userid) AND ((YEAR(a.ts)||LPAD(MONTH(a.ts),2,'0')) = b.yearmonth)
+       JOIN prod_games.arcade.acr_table c 
+       ON (a.userid = c.userid)
        WHERE a.sessionid IN (SELECT sessionid
                              FROM cna_journey
                              GROUP BY 1)
-       GROUP BY 1,2,3,4,5
+       GROUP BY 1,2,3,4,5,6
        UNION ALL
        SELECT
         a.userid
@@ -37,13 +39,16 @@ WITH cna_journey AS
         ,a.ts
         ,CASE WHEN a.game_name LIKE 'Smashy%' THEN 'Smashy Pinata' ELSE a.game_name END AS location
         ,b.segment
+        ,CASE WHEN a.sessionid = c.sessionid THEN 'Yes' ELSE 'No' END AS acr_or_not
        FROM prod_games.arcade.game_open a -- game open
        JOIN prod_games.arcade.engagement_segments b
        ON (a.userid = b.userid) AND ((YEAR(a.ts)||LPAD(MONTH(a.ts),2,'0')) = b.yearmonth)
+       JOIN prod_games.arcade.acr_table c 
+       ON (a.userid = c.userid)
        WHERE a.sessionid IN (SELECT sessionid
                           FROM cna_journey
                           GROUP BY 1)
-       GROUP BY 1,2,3,4,5
+       GROUP BY 1,2,3,4,5,6
        UNION ALL
        SELECT
         a.userid
@@ -51,13 +56,16 @@ WITH cna_journey AS
         ,a.ts
         ,CASE WHEN a.game_name LIKE 'Smashy%' THEN 'Smashy Pinata' ELSE a.game_name END AS location
         ,b.segment
+        ,CASE WHEN a.sessionid = c.sessionid THEN 'Yes' ELSE 'No' END AS acr_or_not
        FROM prod_games.arcade.game_start a -- game start
        JOIN prod_games.arcade.engagement_segments b
        ON (a.userid = b.userid) AND ((YEAR(a.ts)||LPAD(MONTH(a.ts),2,'0')) = b.yearmonth)
+       JOIN prod_games.arcade.acr_table c 
+       ON (a.userid = c.userid)
        WHERE a.sessionid IN (SELECT sessionid
                            FROM cna_journey
                            GROUP BY 1)
-       GROUP BY 1,2,3,4,5
+       GROUP BY 1,2,3,4,5,6
        UNION ALL
        SELECT
         a.userid
@@ -65,13 +73,16 @@ WITH cna_journey AS
         ,a.ts
         ,'ACR' AS location
         ,b.segment
+        ,CASE WHEN a.sessionid = c.sessionid THEN 'Yes' ELSE 'No' END AS acr_or_not
        FROM prod_games.arcade.ACR a -- ACR
        JOIN prod_games.arcade.engagement_segments b
        ON (a.userid = b.userid) AND ((YEAR(a.ts)||LPAD(MONTH(a.ts),2,'0')) = b.yearmonth)
+       JOIN prod_games.arcade.acr_table c 
+       ON (a.userid = c.userid)
        WHERE a.sessionid IN (SELECT sessionid
                            FROM cna_journey
                            GROUP BY 1)
-       GROUP BY 1,2,3,4,5
+       GROUP BY 1,2,3,4,5,6
        UNION ALL
        SELECT
         a.userid
@@ -79,24 +90,35 @@ WITH cna_journey AS
         ,a.ts
         ,a.stunt_name AS location
         ,b.segment
+        ,CASE WHEN a.sessionid = c.sessionid THEN 'Yes' ELSE 'No' END AS acr_or_not
        FROM prod_games.arcade.stunt_open a -- stunts
        JOIN prod_games.arcade.engagement_segments b
        ON (a.userid = b.userid) AND ((YEAR(a.ts)||LPAD(MONTH(a.ts),2,'0')) = b.yearmonth)
+       JOIN prod_games.arcade.acr_table c 
+       ON (a.userid = c.userid)
        WHERE a.sessionid IN (SELECT sessionid
                              FROM cna_journey
                              GROUP BY 1)
-       GROUP BY 1,2,3,4,5)
- GROUP BY 1,2,3,4,5)
- SELECT
-    action_sequence
-    ,location
+       GROUP BY 1,2,3,4,5,6)
+ GROUP BY 1,2,3,4,5,6)
+,user_journey AS
+(SELECT
+    ts::DATE as date
+    ,userid
+    ,LISTAGG(location, ', ') within GROUP (ORDER BY action_sequence,ts ASC) AS journey
     ,segment
-    ,COUNT(DISTINCT a.userid) AS users
-FROM journey_data a
-JOIN cna_journey b ON (a.userid = b.userid)
-GROUP BY 1,2,3
-HAVING users >= 50
-ORDER BY action_sequence ASC;
+    ,acr_or_not
+ FROM journey_data
+ WHERE action_sequence <= 20
+ GROUP BY date,userid,segment,acr_or_not)
+ SELECT
+    date
+    ,journey
+    ,segment
+    ,acr_or_not
+    ,COUNT(DISTINCT userid) AS users
+ FROM user_journey
+ GROUP BY 1,2,3,4;
 
 -- REPORTING schema
 USE DATABASE prod_games;
