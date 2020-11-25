f96d5c64a672d75160825e7415e30c77ac8aeebe
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-11-25 10:20:43 -0500

    Create at_distant_lands.sql

---
+++ stunts v1.0/at_distant_lands.sql
@@ -0,0 +1,63 @@
+use database prod_games;
+use schema arcade;
+use warehouse wh_default;
+
+-- # of users who have HBO Max
+select count(distinct userid)
+from prod_games.arcade.gdb_gotreward
+where title is not null
+and (title = 'Scientist Bubblegum'
+    or title = 'Olive'
+    or title = 'Young Marcy'
+    or title = 'Sheriff BMO'
+    or title = 'Spacesuit BMO'
+    or title = 'Glass Boy'
+    or title = 'See-Through Princess'
+    or title = 'Y-5'
+    or title = 'Mr. M'
+    or title = 'Young Finn & Jake'
+    );
+    
+-- # of users who collected x distinct figures for AT Distant Lands island
+with figure_list as
+    (select distinct
+        userid
+        ,count(distinct title) as distinct_figures
+     from gdb_gotreward
+     where title is not null
+     and (title = 'Scientist Bubblegum'
+           or title = 'Olive'
+           or title = 'Young Marcy'
+           or title = 'Sheriff BMO'
+           or title = 'Spacesuit BMO'
+           or title = 'Glass Boy'
+           or title = 'See-Through Princess'
+           or title = 'Y-5'
+           or title = 'Mr. M'
+           or title = 'Young Finn & Jake'
+         )
+     group by 1
+    )
+,count_users as
+    (select distinct
+        distinct_figures
+        ,case when distinct_figures = 1 then count(distinct userid)
+            when distinct_figures = 2 then count(distinct userid)
+            when distinct_figures = 3 then count(distinct userid)
+            when distinct_figures = 4 then count(distinct userid)
+            when distinct_figures = 5 then count(distinct userid)
+            when distinct_figures = 6 then count(distinct userid)
+            when distinct_figures = 7 then count(distinct userid)
+            when distinct_figures = 8 then count(distinct userid)
+            when distinct_figures = 9 then count(distinct userid)
+            when distinct_figures = 10 then count(distinct userid)
+            else 'not valid'
+            end as num_users
+     from figure_list
+     group by 1
+    )
+select
+    distinct_figures
+    ,round(num_users) as num_users
+from count_users
+group by 1,2;
