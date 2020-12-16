d7a09da2076a63933a0f8e95683b40bd28f2b079
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2020-12-16 14:12:46 -0500

    Update segment_cna_lifetime.sql

--- segments v1.0/segment_cna_lifetime.sql
+++ segments v1.0/segment_cna_lifetime.sql
@@ -9,189 +9,93 @@ select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid in (select userid from prod_games.arcade.game_start)
+     and userid not in (select userid from prod_games.arcade.acr_table)
+     and userid not in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
 
 -- segment by game + ACR
 select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid in (select userid from prod_games.arcade.game_start)
+     and userid in (select userid from prod_games.arcade.acr_table)
+     and userid not in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
 
 -- segment by game + stunt
 select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid in (select userid from prod_games.arcade.game_start)
+     and userid not in (select userid from prod_games.arcade.acr_table)
+     and userid in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
 
 -- segment by game + ACR + stunt
 select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid in (select userid from prod_games.arcade.game_start)
+     and userid in (select userid from prod_games.arcade.acr_table)
+     and userid in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
 
 -- segment by ACR
 select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid not in (select userid from prod_games.arcade.game_start)
+     and userid in (select userid from prod_games.arcade.acr_table)
+     and userid not in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
 
 -- segment by ACR + stunt
 select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid not in (select userid from prod_games.arcade.game_start)
+     and userid in (select userid from prod_games.arcade.acr_table)
+     and userid in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
 
 -- segment by stunt
 select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
-
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid not in (select userid from prod_games.arcade.game_start)
+     and userid not in (select userid from prod_games.arcade.acr_table)
+     and userid in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
+    
 -- segment by disengaged
 select
     count(distinct userid) as num_users
     ,count(distinct sessionid) as num_sessions
 from prod_games.arcade.apprunning
-where country = 'US' 
-and userid in (
-  select userid
-  from prod_games.arcade.first_played_date
-  where start_date >= '3/4/2019'
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.game_start
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.acr_table
-)
-and userid not in (
-  select userid
-  from prod_games.arcade.stunt_open
-);
+where country = 'US'
+and (userid in (select userid from prod_games.arcade.first_played_date where start_date >= '3/4/2019')
+     and userid not in (select userid from prod_games.arcade.game_start)
+     and userid not in (select userid from prod_games.arcade.acr_table)
+     and userid not in (select userid from prod_games.arcade.stunt_open where stunt_name != 'Onboarding Video Stunt')
+    );
