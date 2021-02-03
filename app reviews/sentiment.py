c816cf7daa4ee799e2b7ae37bb69b1de7c244556
Cynthia He <8933832+cynthhe@users.noreply.github.com>
2021-02-03 16:52:59 -0500

    Update sentiment.py

--- app reviews/sentiment.py
+++ app reviews/sentiment.py
@@ -2,8 +2,9 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 
 analyzer = SentimentIntensityAnalyzer()
-df_coc = pd.read_csv("./clash-of-clans.csv", encoding="utf-8")
+df_coc = pd.read_csv("./cna-app-reviews.csv", encoding="utf-8")
 df_coc.head()
+
 
 def calc_vader_sentiment(text):
     sentiment = 1
@@ -11,20 +12,20 @@ def calc_vader_sentiment(text):
     vs = analyzer.polarity_scores(str(text))
     compound = vs['compound']
 
-    if(compound == 0):
+    if compound == 0:
         sentiment = -1
-    elif(compound >= 0.05):
+    elif compound >= 0.05:
         sentiment = 2
-    elif(compound <= -0.05):
+    elif compound <= -0.05:
         sentiment = 0
 
     return sentiment
 
+
 df = pd.DataFrame({
     'uid': list(df_coc.index.values),
-    'text': df_coc['Content'],
-    'label': [calc_vader_sentiment(x) for x in df_coc['Content']]
+    'text': df_coc['review'],
+    'label': [calc_vader_sentiment(x) for x in df_coc['review']]
 })
 
-df.head(10)
 df.to_csv("sentiment_result.csv", encoding='utf8', index=False)
